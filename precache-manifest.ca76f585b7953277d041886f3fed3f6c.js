c351e44b6400909a9b945ee8453afb199b0f232f
Gnana Ganesh <gnanaganesh1999@gmail.com>
2020-11-18 22:29:09 +0530

    Add files via upload

---
+++ precache-manifest.ca76f585b7953277d041886f3fed3f6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe295631ec0f952613b59ba6e667b82",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "cf4a7667894e33393794",
+    "url": "/pomodoro/static/css/main.6848d9ca.chunk.css"
+  },
+  {
+    "revision": "ebf43c08b140bac1ed66",
+    "url": "/pomodoro/static/js/2.4e44b830.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/pomodoro/static/js/2.4e44b830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf4a7667894e33393794",
+    "url": "/pomodoro/static/js/main.09cd1d0c.chunk.js"
+  },
+  {
+    "revision": "7ee2e253b3c617fdb2ba",
+    "url": "/pomodoro/static/js/runtime-main.ae83913c.js"
+  }
+]);
